e6daae522066bc19fe273c7726f5cb6fc89afbb5
Krum Arnaudov <krum.arnaudov@amplifyanalytix.com>
2021-07-21 17:17:49 +0300

    Experience collections

--- meta/dummy_estimators.py
+++ meta/dummy_estimators.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import numpy as np
+from prophet import Prophet
+from prophet.diagnostics import cross_validation, performance_metrics
+from prophet.plot import add_changepoints_to_plot
+import sys
+import traceback
+from tutorials.helper_funcs import load_energy_data, load_temperature, merge_datasets, resample_as_needed
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 class SameForecaster:
     """A class to return the same value for a time series with very few data points.
@@ -76,3 +86,210 @@ class SameForecaster:
           future_df_copy["yhat"] = future_df_copy["yhat"].fillna(0)
 
         return future_df_copy
+
+
+class SimpleProphet(Prophet):
+  
+  """
+  A simple class including Covid modelling and US Holidays.
+  In addition, predictions are clipped at zero to not allow for negative values.
+  
+  """
+  
+  def __init__(self, add_lockdown = True, *args, **kwargs) -> None:
+    super().__init__(*args, **kwargs)
+    
+    if add_lockdown:
+      lockdown1 = pd.DataFrame({
+        'holiday': 'lockdown1',
+        'ds': pd.to_datetime(["2020-03-11"]),
+        'upper_window': 111,
+        'lower_window':0
+      })
+      
+      lockdown2 = pd.DataFrame({
+        'holiday': 'lockdown2',
+        'ds': pd.to_datetime(["2020-07-01"]),
+        'upper_window': 91,
+        'lower_window':0
+      })
+      
+      lockdown3 = pd.DataFrame({
+        'holiday': 'lockdown3',
+        'ds': pd.to_datetime(["2020-10-01"]),
+        'upper_window': 135,
+        'lower_window':0
+      })
+      
+      lockdowns = pd.concat([lockdown1, lockdown2, lockdown3])
+      self.holidays = lockdowns
+      
+    self.add_country_holidays(country_name='US')
+      
+  def predict(self, *args, **kwargs):
+    forecast = super(SimpleProphet, self).predict(*args, **kwargs)
+    
+    for y in ['yhat', 'yhat_lower', 'yhat_upper']:
+      forecast[y] = forecast[y].clip(lower=0)
+      
+    return forecast
+  
+  
+class ProphetStepWise(Prophet):
+  
+    def __init__(self, add_lockdown = True, *args, **kwargs) -> None:
+      super().__init__(*args, **kwargs)
+    
+      if add_lockdown:
+        lockdown1 = pd.DataFrame({
+          'holiday': 'lockdown1',
+          'ds': pd.to_datetime(["2020-03-11"]),
+          'upper_window': 111,
+          'lower_window':0
+        })
+        
+        lockdown2 = pd.DataFrame({
+          'holiday': 'lockdown2',
+          'ds': pd.to_datetime(["2020-07-01"]),
+          'upper_window': 91,
+          'lower_window':0
+        })
+        
+        lockdown3 = pd.DataFrame({
+          'holiday': 'lockdown3',
+          'ds': pd.to_datetime(["2020-10-01"]),
+          'upper_window': 135,
+          'lower_window':0
+        })
+      
+        lockdowns = pd.concat([lockdown1, lockdown2, lockdown3])
+        self.holidays = lockdowns
+      
+      self.add_country_holidays(country_name='US')
+    
+    def fit(self, df, **kwargs):
+        m = super().fit(df, **kwargs)
+        if self.growth == 'flat' and self.changepoints is not None:
+            kinit = self.stepwise_growth_init(self.history, self.changepoints)
+            self.params['m_'] = kinit[1]
+        return self
+        
+
+    @staticmethod
+    def stepwise_growth_init(df, changepoints):
+        k = 0
+        if len(changepoints) == 0:
+            k = 0
+            m = df['y_scaled'].mean()
+        else:
+            m = []
+            last_cp = pd.Timestamp.min
+            for cp in changepoints:
+                df_  = df[(df.ds > last_cp) & (df.ds <= cp)]['y_scaled']
+                m.append(df_.mean())
+                last_cp = cp
+            cp = changepoints.iloc[-1]
+            df_  = df[df.ds > cp]['y_scaled']
+            m.append(df_.mean())
+        return k, m
+    
+    @staticmethod
+    def stepwise_trend(t, m, changepoint_ts):
+        if changepoint_ts[0] == 0:
+            m_t = m * np.ones_like(t)
+        else:
+            m_t = np.ones_like(t)
+            last_t_s = changepoint_ts[0]-1
+            for s, t_s in enumerate(changepoint_ts):
+                indx = (t <= t_s) & (t > last_t_s)
+                m_t[indx] = m[s]
+                last_t_s = t_s
+
+            t_s = changepoint_ts[-1]
+            indx = (t > t_s) 
+            m_t[indx] = m[-1]
+        return m_t
+    
+    def sample_predictive_trend(self, df, iteration):
+        """Simulate the trend using the extrapolated generative model.
+        Parameters
+        ----------
+        df: Prediction dataframe.
+        iteration: Int sampling iteration to use parameters from.
+        Returns
+        -------
+        np.array of simulated trend over df['t'].
+        """
+        k = self.params['k'][iteration]
+        m = self.params['m'][iteration]
+        deltas = self.params['delta'][iteration]
+
+        t = np.array(df['t'])
+        T = t.max()
+
+        # New changepoints from a Poisson process with rate S on [1, T]
+        if T > 1:
+            S = len(self.changepoints_t)
+            n_changes = np.random.poisson(S * (T - 1))
+        else:
+            n_changes = 0
+        if n_changes > 0:
+            changepoint_ts_new = 1 + np.random.rand(n_changes) * (T - 1)
+            changepoint_ts_new.sort()
+        else:
+            changepoint_ts_new = []
+
+        # Get the empirical scale of the deltas, plus epsilon to avoid NaNs.
+        lambda_ = np.mean(np.abs(deltas)) + 1e-8
+
+        # Sample deltas
+        deltas_new = np.random.laplace(0, lambda_, n_changes)
+
+        # Prepend the times and deltas from the history
+        changepoint_ts = np.concatenate((self.changepoints_t,
+                                         changepoint_ts_new))
+        deltas = np.concatenate((deltas, deltas_new))
+
+        if self.growth == 'linear':
+            trend = self.piecewise_linear(t, deltas, k, m, changepoint_ts)
+        elif self.growth == 'logistic':
+            cap = df['cap_scaled']
+            trend = self.piecewise_logistic(t, cap, deltas, k, m,
+                                            changepoint_ts)
+        elif self.growth == 'flat':
+            trend = self.stepwise_trend(t, self.params['m_'], self.changepoints_t)
+
+        return trend * self.y_scale + df['floor']
+    
+    def predict_trend(self, df):
+        """Predict trend using the prophet model.
+        Parameters
+        ----------
+        df: Prediction dataframe.
+        Returns
+        -------
+        Vector with trend on prediction dates.
+        """
+        k = np.nanmean(self.params['k'])
+        m = np.nanmean(self.params['m'])
+        deltas = np.nanmean(self.params['delta'], axis=0)
+
+        t = np.array(df['t'])
+        if self.growth == 'linear':
+            trend = self.piecewise_linear(t, deltas, k, m, self.changepoints_t)
+        elif self.growth == 'logistic':
+            cap = df['cap_scaled']
+            trend = self.piecewise_logistic(
+                t, cap, deltas, k, m, self.changepoints_t)
+        elif self.growth == 'flat':
+            # constant trend
+            trend = self.stepwise_trend(t, self.params['m_'], self.changepoints_t)
+        return trend * self.y_scale + df['floor']
+      
+    def predict(self, *args, **kwargs):
+      forecast = super(ProphetStepWise, self).predict(*args, **kwargs)
+    
+      for y in ['yhat', 'yhat_lower', 'yhat_upper']:
+        forecast[y] = forecast[y].clip(lower=0)
+      
+      return forecast
